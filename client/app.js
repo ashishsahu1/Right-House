0bd5d33aab96d74491dc6c4fe1ab5b6b9f8d4513
Ashish Sahu <ashishsahu1999@hotmail.com>
2020-11-01 19:59:51 +0530

    Final Commit

--- client/app.js
+++ client/app.js
@@ -27,7 +27,7 @@ function getBathValue() {
     var estPrice = document.getElementById("uiEstimatedPrice");
   
      var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-    //var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+    // var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
   
     $.post(url, {
         total_sqft: parseFloat(sqft.value),
